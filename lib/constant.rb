bc6bdba1b0b4d8b743415a2ff340ad91bf612991
M111q <32622203+M111q@users.noreply.github.com>
2018-01-23 11:09:00 +0100

    Add files via upload

--- lib/constant.rb
+++ lib/constant.rb
@@ -1,4 +1,6 @@
-class Const
+# Zawiera stale wykorzystywane w programie
+require_relative '../lib/game.rb'
+class Constants
   SIZE = 10
   AXE_LETTERS = %w( A B C D E F G H I J ).freeze
   AXE_DIGITS = %w( 1 2 3 4 5 6 7 8 9 10 ).freeze
@@ -6,4 +8,48 @@ class Const
   MISS_CHAR = '-'.freeze
   NO_SHOT_CHAR = '·'.freeze
   HISTORY_LENGTH = 8
+  STATES = %i(initialized ready error terminated game_over).freeze
+
+
+
+  SHIPS_DEFS = [
+    { size: 4, type: 'Battleship' },
+    { size: 4, type: 'Battleship' },
+    { size: 5, type: 'Aircraft carrier' }
+  ].freeze
+end
+# Klasa wyboru gry
+class TypeOfGame
+  def key_validation(selected_game = ClassicGame)
+    game = selected_game.new
+    game.key_validation
+  end
+end
+# ExtraGame class
+class ExtraGame < TypeOfGame
+  def key_validation #changeable_size_board_key_validation
+    decimal_number = $board_size/10
+    digit = $board_size%10
+  if $board_size < 10
+    /^[A-#{($board_size + 64).chr}]([1-#{$board_size}])$/
+  elsif ($board_size >= 10 && $board_size < 27) #pojedyncze litery
+    /^([A-#{($board_size + 64).chr}])([1-9]|[1-#{decimal_number}][0-#{digit}])$/
+  else #podwojne litery np 30 = 26 + 4 = [A][A - D] = 30 - 26 = 4
+    first_letter = ((($board_size / 26.3).to_i) + 64).chr
+    first_letter_precursor = ((($board_size / 26.3).to_i) + 64 - 1).chr # poprzednik first_lettera
+    second_letter = ((($board_size % 26.2).round) + 64).chr
+    /^(!@|[A-Z]|[A][A-#{second_letter}]|[@-#{first_letter_precursor}][A-Z]|[#{first_letter}][A-#{second_letter}])([1-9]|[1-#{decimal_number}][0-#{digit}])$/
+    # pierwsze dla pojedynczych liter @@@@@@@@@@@@@
+    # drugie dla zakresu [A][*] od AA do A*
+    # trzecie dla zakresu [*-1][A-Z]
+    # czwarte dla zakresu [*][A-*] od AA do A
+  end
+  end
+end
+# ClassicGame class
+class ClassicGame < TypeOfGame
+  # standard_board_size_key_validation
+  def key_validation
+    /^[A-J]([1-9]|10)$/
+  end
 end
